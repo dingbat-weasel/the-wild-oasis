165643cdcca5d5468c06a749fe13ac32c61f6e27
Isaac Perk <izek@rill.local>
2023-09-16 14:52:17 -0700

    setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,33 @@
-import { styled } from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 import GlobalStyles from './styles/GlobalStyles';
 
-// This implements the styling for the h1 styled component using ES6 feature 'tagged template literals'
-// Variable must start with uppercase because it is a React Component
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  box-shadow: var(--shadow-sm)
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
-  margin: 20px;
-
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
-
-// Convention to call StyledApp
-const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20px;
-`;
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <>
-      {/* {Global Styles component needs to be added to component tree but cannot
-      accept any children} */}
       <GlobalStyles />
-      <StyledApp>
-        <H1>The Wild Oasis</H1>
-        <Button onClick={() => alert('Check in')}>Check In</Button>
-        <Button onClick={() => alert('Check out')}>Check Out</Button>
-        <Input type='number' placeholder='number of guests' />
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to='dashboard' />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='users' element={<Users />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='account' element={<Account />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,35 @@
+// This implements the styling for the h1 styled component using ES6 feature 'tagged template literals'
+
+import { css, styled } from 'styled-components';
+
+// // using 'css' here allows for syntaxt highlighting and js execution
+// const test = css`
+//   text-align: center;
+//   ${10 > 5 && 'background-color: yellow'};
+// `;
+
+// Variable must start with uppercase because it is a React Component
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === 'h1' &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+  ${(props) =>
+    props.as === 'h2' &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+    ${(props) =>
+    props.as === 'h3' &&
+    css`
+      font-size: 2rem;
+      font-weight: 500;
+    `}
+
+  line-height: 1.4rem;
+`;
+
+export default Heading;
