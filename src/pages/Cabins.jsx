86aaebb336e1d743d65e4efb548cdfd77a2b8c00
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-09-22 11:07:44 -0700

    adding form functionality for adding cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,14 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
+
 import CabinTable from '../features/cabins/CabinTable';
+import Button from '../ui/Button';
+import CreateCabinForm from '../features/cabins/CreateCabinForm';
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type='horizontal'>
@@ -14,6 +18,11 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add New Cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
