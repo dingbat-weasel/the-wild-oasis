bc50cbbbf1dd588034374b5e42d65014904d60af
Isaac Perk <izek@rill.local>
2023-09-30 17:28:26 -0700

    sorting in bookings table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,15 +1,35 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // 1. FILTER
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : {
+          field: 'status',
+          value: filterValue,
+          // can include method here for more refined filtering
+        };
+
+  // 2. SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
     // For notes on these queries check useCabins
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    // this is similar to dependency array of useeffect hook; if filter changes it will refetch
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
