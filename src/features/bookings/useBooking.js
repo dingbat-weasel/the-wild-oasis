18cde9dbb68c415d3aa1c2eb40392be4c58bc4db
Isaac Perk <izek@rill.local>
2023-09-30 18:49:27 -0700

    single booking page

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,22 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getBooking } from '../../services/apiBookings';
+
+export function useBooking() {
+  const { bookingId } = useParams();
+
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    // Uniquely identify the data, must be array
+    // If another component wants to use the data, this key can be used to read from cache
+    queryKey: ['booking'],
+    // Function which is responsible for fetching data from API, must return a promise
+    queryFn: () => getBooking(bookingId),
+    retry: false,
+  });
+
+  return { isLoading, error, booking };
+}
---
+++ src/pages/Booking.jsx
@@ -0,0 +1,8 @@
+import BookingDetail from '../features/bookings/BookingDetail';
+
+function Booking() {
+  return <BookingDetail />;
+}
+
+// A page should not fetch data or have side effects, cleaner pages
+export default Booking;
